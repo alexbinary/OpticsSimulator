3b69228dbf47d12fee629aaab0768734e76bf685
Alexandre Bintz <alexandre@bintz.io>
2026-04-11 14:02:22 +0200

    Initial Commit

---
+++ OpticsSimulator/OpticsSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpticsSimulatorApp.swift
+//  OpticsSimulator
+//
+//  Created by Alexandre Bintz on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpticsSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
