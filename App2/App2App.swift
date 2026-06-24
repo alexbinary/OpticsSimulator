22eaa405012c28d3709b1340b84f740d3a57e371
Alexandre Bintz <alexandre@bintz.io>
2026-06-24 09:39:04 +0200

    create target: App2

---
+++ App2/App2App.swift
@@ -0,0 +1,17 @@
+//
+//  App2App.swift
+//  App2
+//
+//  Created by Alexandre Bintz on 24/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct App2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App2
+//
+//  Created by Alexandre Bintz on 24/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
