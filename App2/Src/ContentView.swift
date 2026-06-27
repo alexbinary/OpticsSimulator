3c7c620f1cfffd901f4bbc4e1d7cb33c3a9e7002
Alexandre Bintz <alexandre@bintz.io>
2026-06-27 15:36:01 +0200

    axis and cursor

--- App2/Src/ContentView.swift
+++ App2/Src/ContentView.swift
@@ -5,80 +5,94 @@ import SwiftUI
 struct ContentView: View {
     
     
+    @State private var canvasSize: CGSize = .zero
+    @State private var mouse: CGPoint = .zero
+    
     @State private var viewportCenter: CGPoint = .zero
     @State private var viewportRotation: Angle = .zero
     @State private var viewportZoom: CGFloat = 1
     
-    @State private var canvasSize: CGSize = .zero
     
-
     var body: some View {
         
-        ZStack {
-        
-            Canvas { context, size in
+        VStack {
+            
+            ZStack {
                 
-                // move origin to canvas center, y positive up
-                context.translateBy(x: size.width/2, y: size.height/2)
-//                context.translateBy(x: 0, y: size.height)
-                context.scaleBy(x: 1, y: -1)
-                
-                let renderer = Renderer(
-                    context: context,
-                    renderSize: size,
-                    viewportCenter: viewportCenter,
-                    viewportRotation: viewportRotation,
-                    viewportZoom: viewportZoom
-                )
-                renderer.draw(
-                    Lense(
-                        type: .convergent,
-                        diameter: 100,
-                        focalLength: 10,
-                        position: CGPoint(x: 0, y: 0),
-                        rotation: .degrees(45)
+                Canvas { context, size in
+                    
+                    let renderer = Renderer(
+                        context: context,
+                        canvasSize: size,
+                        viewportCenter: viewportCenter,
+                        viewportRotation: viewportRotation,
+                        viewportZoom: viewportZoom,
+                        mouse: localMouse
                     )
-                )
-                
-            }
-            .background(
-                GeometryReader { proxy in
-                    Color.clear
-                        .onChange(of: proxy.size, initial: true) { _, newSize in
-                            // update canvas size safely outside of drawing
-                            self.canvasSize = newSize
-                        }
-                }
-            )
-            MouseEventsArea(
-                onMove: { point in
-                    print("canvas size: \(canvasSize)")
-                    viewportCenter = CGPoint(
-                        x: point.x - canvasSize.width/2,
-                        y: point.y - canvasSize.height/2,
-                    )
-                },
-                onScroll: { dx, dy, modifiers, phase, momentumPhase in
-                    if modifiers.contains(.option) {
-                        if momentumPhase == [] {
-                            viewportZoom += dy*0.001*viewportZoom
-                        }
-                    } else {
-                        viewportCenter = CGPoint(
-                            x: viewportCenter.x + dx,
-                            y: viewportCenter.y - dy
+                    renderer.render(
+                        lense: Lense(
+                            type: .convergent,
+                            diameter: 100,
+                            focalLength: 10,
+                            position: CGPoint(x: 0, y: 0),
+                            rotation: .degrees(45)
                         )
-                    }
-                },
-                onRotate: { delta, point in
-                    viewportRotation += delta
-                },
-                onPinch: { delta, point in
-                    viewportZoom += delta
+                    )
                 }
-            )
+                .background(
+                    GeometryReader { proxy in
+                        Color.clear
+                            .onChange(of: proxy.size, initial: true) { _, newSize in
+                                self.canvasSize = newSize
+                            }
+                    }
+                )
+                MouseEventsArea(
+                    onMove: { point in
+                        mouse = point
+                    },
+                    onScroll: { dx, dy, modifiers, phase, momentumPhase in
+                        if modifiers.contains(.option) {
+                            if momentumPhase == [] {
+                                viewportZoom += dy*viewportZoom*0.01
+                            }
+                        } else {
+                            viewportCenter = CGPoint(
+                                x: viewportCenter.x + dx,
+                                y: viewportCenter.y - dy
+                            )
+                        }
+                    },
+                    onRotate: { delta, point in
+                        viewportRotation += delta
+                    },
+                    onPinch: { delta, point in
+                        viewportZoom += delta*viewportZoom*0.5
+                    }
+                )
+            }
+            
+            Text("\(localMouse.x); \(localMouse.y)")
         }
         .frame(minWidth: 1600, minHeight: 800)
+    }
+    
+    
+    var localMouse: CGPoint {
+        
+        return mouse
+            .applying(.init(
+                translationX: -canvasSize.width/2, y: -canvasSize.height/2)
+            )
+            .applying(.init(
+                translationX: -viewportCenter.x, y: -viewportCenter.y)
+            )
+            .applying(.init(
+                rotationAngle: -viewportRotation.radians)
+            )
+            .applying(.init(
+                scaleX: 1/viewportZoom, y: 1/viewportZoom)
+            )
     }
 }
 
@@ -107,39 +121,107 @@ struct Lense {
 class Renderer {
     
     var context: GraphicsContext
-    let renderSize: CGSize
+    let canvasSize: CGSize
     
     let viewportCenter: CGPoint
     let viewportRotation: Angle
     let viewportZoom: CGFloat
     
+    let mouse: CGPoint
+    let viewportBounds: CGRect
+    
     
     init(
         context: GraphicsContext,
-        renderSize: CGSize,
+        canvasSize: CGSize,
         viewportCenter: CGPoint,
         viewportRotation: Angle,
-        viewportZoom: CGFloat
+        viewportZoom: CGFloat,
+        mouse: CGPoint
     ) {
         self.context = context
-        self.renderSize = renderSize
+        self.canvasSize = canvasSize
         self.viewportCenter = viewportCenter
         self.viewportRotation = viewportRotation
         self.viewportZoom = viewportZoom
+        self.mouse = mouse
+        
+        let center = viewportCenter.applying(.init(rotationAngle: -viewportRotation.radians))
+        
+        let dim = max(canvasSize.width, canvasSize.height)
+        
+        let minX: CGFloat = (-dim-center.x)/viewportZoom
+        let maxX: CGFloat = (dim-center.x)/viewportZoom
+        
+        let minY: CGFloat = (-dim-center.y)/viewportZoom
+        let maxY: CGFloat = (dim-center.y)/viewportZoom
+        
+        self.viewportBounds = CGRect(
+            origin: CGPoint(x: minX, y: minY),
+            size: CGSize(width: maxX-minX, height: maxY-minY)
+        )
+    }
+    
+    
+    func lineWidth(_ width: CGFloat) -> CGFloat {
+        
+        return width/viewportZoom
+    }
+    
+    
+    func render(lense: Lense) {
+        
+        context.scaleBy(x: 1, y: -1)
+        context.translateBy(x: 0, y: -canvasSize.height)
+        context.translateBy(x: canvasSize.width/2, y: canvasSize.height/2)
+        
+        context.translateBy(x: viewportCenter.x, y: viewportCenter.y)
+        context.rotate(by: viewportRotation)
+        context.scaleBy(x: viewportZoom, y: viewportZoom)
+        drawAxis()
+
+        draw(lense)
+        
+        drawCursor()
+    }
+    
+    
+    func drawAxis() {
+        
+        var path = Path()
+        let color: Color = .white
+        
+        path.move(to: CGPoint(x: viewportBounds.minX, y: 0))
+        path.addLine(to: CGPoint(x: viewportBounds.maxX, y: 0))
+        
+        path.move(to: CGPoint(x: 0, y: viewportBounds.minY))
+        path.addLine(to: CGPoint(x: 0, y: viewportBounds.maxY))
+        
+        context.stroke(path, with: .color(color), lineWidth: lineWidth(1))
+    }
+    
+    
+    func drawCursor() {
+        
+        var path = Path()
+        let color: Color = .white
+        
+        path.move(to: CGPoint(x: viewportBounds.minX, y: mouse.y))
+        path.addLine(to: CGPoint(x: viewportBounds.maxX, y: mouse.y))
+        
+        path.move(to: CGPoint(x: mouse.x, y: viewportBounds.minY))
+        path.addLine(to: CGPoint(x: mouse.x, y: viewportBounds.maxY))
+        
+        context.stroke(path, with: .color(color), lineWidth: lineWidth(1))
     }
     
     
     func draw(_ lense: Lense) {
         
-        // apply viewport
-        context.translateBy(x: viewportCenter.x, y: viewportCenter.y)
-        context.rotate(by: viewportRotation)
-        context.scaleBy(x: viewportZoom, y: viewportZoom)
-
-        // move to local coordinates
+        var context = context
         context.translateBy(x: lense.position.x, y: lense.position.y)
         context.rotate(by: lense.rotation)
-
+        
         let h: CGFloat = lense.diameter/2
         let f: CGFloat = lense.focalLength
         let a: CGFloat = 5
@@ -163,7 +245,7 @@ class Renderer {
         path.move(to: CGPoint(x: 0, y: -h))
         path.addLine(to: CGPoint(x: -a, y: -h+a))
         
-        context.stroke(path, with: .color(color), lineWidth: 2)
+        context.stroke(path, with: .color(color), lineWidth: lineWidth(2))
         
         // connect focal points
         path.move(to: CGPoint(x: 0, y: h))
@@ -173,7 +255,7 @@ class Renderer {
         path.addLine(to: CGPoint(x: 0, y: h))
         
         context.stroke(path, with: .color(color), style: StrokeStyle(
-            lineWidth: 1,
+            lineWidth: lineWidth(1),
             dash: [4, 4]
         ))
     }
